8f678f8585d5685b257c2ee325e59df81cba54a7
Paulo Henrique <paulo.silva@southsystem.com.br>
2021-12-05 21:14:45 -0300

    feat: testing

--- src/service/global.ts
+++ src/service/global.ts
@@ -2,7 +2,7 @@ import md5 from "md5";
 
 const ts = Number(new Date());
 const privateKey = process.env.REACT_APP_PRIVATE_KEY as string
-const apikey = process.env.REACT_APP_API_KEY
+const apikey = "aa5a981963fa6f57eeb88dc2be8fc61b"
 
 const hash = md5(ts + privateKey + apikey);
 
