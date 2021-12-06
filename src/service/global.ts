6b7e148c27a0e8fe9c32bc299f71c8a92469dd89
Paulo Henrique <paulo.silva@southsystem.com.br>
2021-12-05 21:04:51 -0300

    feat: add change

--- src/service/global.ts
+++ src/service/global.ts
@@ -1,8 +1,8 @@
 import md5 from "md5";
 
 const ts = Number(new Date());
-const privateKey = process.env.REACT_APP_PRIVATE_KEY as string
-const apikey = process.env.REACT_APP_API_KEY as string
+const privateKey = process.env.REACT_APP_API_KEY as string
+const apikey = process.env.REACT_APP_PRIVATE_KEY as string
 
 const hash = md5(ts + privateKey + apikey);
 
